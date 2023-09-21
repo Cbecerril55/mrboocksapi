79a0a4335ec5c17a4953db70e9ffe868047906d7
AstroGaze <99556112+AstroGaze@users.noreply.github.com>
2023-09-21 15:26:48 -0500

    .

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const app = express();
 const database = require("./db");
 
 app.get('/', (req, res) => {
-   
+   res.send('List all data');
 });
 
 app.get('/autores', (req, res) => {
-   
+    res.send('List all data');
 });
 
 app.get('/usuarios', (req, res) => {
-   
+    res.send('List all data');
 });
 
 app.get('/libros', (req, res) => {
-   
+    res.send('List all data');
 });
 
 app.get('/prestamos', (req, res) => {
-   
+    res.send('List all data');
 });
 
 
